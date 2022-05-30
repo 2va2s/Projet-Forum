902ba4a8a9debdd26b69403ee5aa5138c30b627a
WSAIOUDI <wassim.saioudi@ynov.com>
2022-05-30 15:43:36 +0200

    file js replaced

--- static/js/accueil.js
+++ static/js/accueil.js
@@ -13,7 +13,7 @@ fetch("/posts").then(function (response) {
 let conn = document.getElementById("connected")
 conn = conn
 console.log(conn)
-if (conn.innerHTML == " Connecté en tant que [] ") {
+if (conn.innerHTML == " Connecté en tant que [] ") { 
     conn.innerHTML = "Non connecté"
     document.getElementById("logout").style.display = 'none';
     document.getElementById("login").style.display = 'block';
