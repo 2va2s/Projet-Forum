67ff725b3af56adee75e3b272345a96b5c660a0b
Emile Bailey <emilebailey@MacBook-Pro-de-Emile.local>
2022-06-01 11:29:21 +0200

    slight bug

--- Forum/login-signin.go
+++ Forum/login-signin.go
@@ -1,11 +1,13 @@
 package Forum
 
 import (
+	"crypto/md5"
 	"database/sql"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
+	"reflect"
 	"text/template"
 
 	"github.com/gorilla/sessions"
@@ -19,6 +21,10 @@ var (
 type UserData struct {
 	Email    []string `json:"Email"`
 	Password []string `json:"Password"`
+}
+
+func checkLogin(db *sql.DB, pseudo string, mdp string) {
+
 }
 
 func HandleHome(w http.ResponseWriter, r *http.Request) {
@@ -54,11 +60,48 @@ func HandleSignin(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		return
 	}
 
-	number, _ := strconv.Atoi(r.Form.Get("Telephone"))
-	CreateUser(db, r.Form.Get("Surnom"), r.Form.Get("Password"), r.Form.Get("Email"), number, "")
+	// number, _ := strconv.Atoi(r.Form.Get("number"))
+	// CreateUser(db, r.Form.Get("pseudo"), encrypt(r.Form.Get("password")), r.Form.Get("mail"), number, "")
+	create(db, "user", User{}, r.Form.Get("pseudo"), encrypt(r.Form.Get("password")), r.Form.Get("mail"), r.Form.Get("number"), "")
+}
+
+func ParseTable(model interface{}, table string) (a string) {
+
+	result := ""
+	e := reflect.ValueOf(model)
+	for i := 0; i < e.NumField(); i++ {
+		varName := e.Type().Field(i).Name
+		result += string(varName) + ", "
+	}
+	result = result[:len(result)-2]
+	return result
+}
+
+func create(db *sql.DB, table string, model interface{}, t ...string) {
+
+	result := "INSERT INTO " + table + " (" + ParseTable(model, "user") + ")" + " VALUES " + "("
+	for i := 0; i < len(t); i++ {
+		result += t[i] + ", "
+		if i < len(t)-1 {
+			result += t[i]
+		}
+	}
+	fmt.Println("1", result)
+
+	result = result[:len(result)-1]
+	result += ")"
+
+	// request, _ := db.Exec(result)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	fmt.Println(result)
+	// return request.LastInsertId()
+
 }
 
 func HandleLogin(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+
 	// r.Form pour recup valeurs de form
 	// db.USer == r.Form
 	if r.URL.Path != "/login" {
@@ -66,16 +109,20 @@ func HandleLogin(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		return
 	}
 
+	///////////////// LOGIN ////////////////
+
 	tmpl, _ := template.ParseFiles("./pages/accueil.html")
 
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, "Error parsing form", 500)
 	}
 
+	// checkLogin(db, r.Form.Get("pseudo"), )
+
 	session, _ := store.Get(r, "cookie-forum")
 
 	if _, ok := r.PostForm["Submit"]; ok {
-		fmt.Println("in")
+		fmt.Println("user logged in")
 		res, _ := json.Marshal(r.PostForm)
 		session.Values["authenticated"] = string(res)
 		session.Save(r, w)
@@ -104,4 +151,13 @@ func checkEmptyField(r *http.Request) string {
 	// 	return "le champs mot de passe doit contenir au moins 4 caractères"
 	// }
 	return ""
+}
+
+func encrypt(pwd string) string {
+
+	hasher := md5.New()
+	hasher.Write([]byte(pwd))
+	a := hex.EncodeToString(hasher.Sum(nil))
+	fmt.Println(a)
+	return a
 }
