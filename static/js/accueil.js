cbeba63303c45bac8a117218ca311db349dd0c2a
WSAIOUDI <wassim.saioudi@ynov.com>
2022-06-02 10:30:37 +0200

    component like vincent + refaire login/signin

--- static/js/accueil.js
+++ static/js/accueil.js
@@ -11,8 +11,31 @@ fetch("/posts").then(function (response) {
 })
 
 let conn = document.getElementById("connected")
-if (conn.innerHTML == " Connecté en tant que [] ") { 
+if (conn.innerHTML == " Connecté en tant que [] ") {
     conn.innerHTML = "Non connecté"
     document.getElementById("logout").style.display = 'none';
     document.getElementById("login").style.display = 'block';
 }
+
+
+
+export const newPost = (post) => {
+    const p = document.createElement('div')
+    p.innerHTML = 
+        `
+            <div class="toto">
+                <p>${post.title}</p>
+            </div>
+        `
+    return p
+}
+
+
+
+
+
+
+
+
+
+
--- static/js/login-signin.js
+++ static/js/login-signin.js
@@ -22,13 +22,10 @@ const checkLoginForm = () => {
             let mdp = document.getElementById('mdp1')
             if (response[i].Pseudo == email.value && response[i].Password == mdp.value) {
                 document.getElementById('subscribe').submit()
-            } else {
-                // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                // §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
-                // CREER MESSAGE QUI DIT IDENTIFIANTS INCORRECT
-                // §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
-                // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                return
             }
+            errorP = document.getElementById("loginError")
+            errorP.innerText = "Identifiants incorrects"
         }
     }
     ).catch(function (err) {
@@ -36,10 +33,28 @@ const checkLoginForm = () => {
     })
 }
 
-const checkRegisterForm = () => {
-    fetch("/users").then(function (response) {
-        return response.json()
-    }).then(function (response) {
-        
-    }
-}
+// const checkRegisterForm = () => {
+//     fetch("/users").then(function (response) {
+//         return response.json()
+//     }).then(function (response) {
+//         // let pseudo = document.getElementById('pseudo2')
+//         // let mdp1 = document.getElementById('password2')
+//         // let mdp2 = document.getElementById('password22')
+//         // let email = document.getElementById('email2')
+//         // let telephone = document.getElementById('telephone')
+//         // if (pseudo.value.lenght < 4) raiseRegisterError("Le mot de passe doit contenir au mois 4 caractères")
+//         // if (pseudo.value.includes(" ") < 4) raiseRegisterError("Le pseudo ne doit pas contenir d'espaces (utiliser un '_' ou un '-'")
+//         // if (mdp1.value != mdp2.value) raiseRegisterError()
+
+
+
+
+//         for (let i in response) {
+//             if (response[i].Pseudo == email.value && response[i].Password == mdp.value) {
+//                 document.getElementById('subscribe').submit()
+//             } else {
+//                 console.log("true")
+//             }
+//         }
+//     })
+// }
