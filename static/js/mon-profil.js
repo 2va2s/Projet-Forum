6aa781d904c283d8eb492fef66d443fe15d926e0
RPOTIGNON <remi.potignon@ynov.com>
2022-06-13 11:17:30 +0200

    profil

--- static/js/mon-profil.js
+++ static/js/mon-profil.js
@@ -8,7 +8,11 @@ let mail = document.getElementById("e-mail");
 let rang = document.getElementById("rang");
 let liste = document.getElementById("liste");
 let listePosts = document.getElementsByClassName("listePosts");
-let déco = document.getElementById("déco")
+let déco = document.getElementById("déco");
+let mesCommentaires = document.getElementById("mesCommentaires");
+let commentaires = document.getElementById("commentaires");
+let badges = document.getElementById("badges");
+let déroul = document.getElementById("scroll")
 
 monCompte.addEventListener("click", () => {
     
@@ -18,24 +22,25 @@ monCompte.addEventListener("click", () => {
         pseudo.style.display = "block";
         mail.style.display = "block";
         rang.style.display = "block"
+        badges.style.display = "block"
         liste.style.display = "none"
-        déco.style.display = "none"
-   
-         
-        
-        
+        déco.style.display = "none";
+        commentaires.style.display = "none"
+        déroul.style.display = "none"   
     }
 )
 
 posts.addEventListener("click", () => {
     
     
-        liste.style.display = "block"
+        déroul.style.display = "block"
         photo.style.display = "none";
         pseudo.style.display = "none";
         mail.style.display = "none";
         rang.style.display = "none";
-        déco.style.display = "none"
+        déco.style.display = "none";
+        commentaires.style.display ="none"
+        badges.style.display ="none"
     } 
 )
 
@@ -46,6 +51,20 @@ déconnexion.addEventListener("click", () => {
         pseudo.style.display = "none";
         mail.style.display = "none";
         rang.style.display = "none";
+        commentaires.style.display = "none"
+        badges.style.display = "none"
+        déroul.style.display = "none"
         
+})
+
+mesCommentaires.addEventListener("click", () => {
+        déroul.style.display = "block"
+        déco.style.display = "none"
+        liste.style.display = "none"
+        photo.style.display = "none";
+        pseudo.style.display = "none";
+        mail.style.display = "none";
+        rang.style.display = "none";
+        badges.style.display = "none"
 })
 
